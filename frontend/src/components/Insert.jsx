703e1c080051293a9b2d2941ebec9f2a07b3a3c2
Pratham01 <cprathmesh01@gmail.com>
2024-05-06 11:27:59 +0530

    commit

--- frontend/src/components/Insert.jsx
+++ frontend/src/components/Insert.jsx
@@ -18,9 +18,9 @@ function Insert() {
     }
 
     for (let [key, value] of formData.entries()) {
-      if(value != ""){
+      if (value != "") {
         serializedFormData[key] = value;
-      }else{
+      } else {
         alert("Please fill all the required info");
         return;
       }
@@ -51,6 +51,8 @@ function Insert() {
       } catch (e) {}
     }
   }, []);
+
+  const validateInput = () => {};
 
   return (
     <>
@@ -89,8 +91,21 @@ function Insert() {
                                 onChange={(e) =>
                                   setUser({ ...user, name: e.target.value })
                                 }
+                                onBlur={(e) => {
+                                  const err = document.getElementById("nameErr");
+                                  if (e.target.value.match(/[1-9]/g)) {
+                                    err.innerHTML =
+                                      "Please enter Alphabates only";
+                                  } else {
+                                    err.innerHTML = "";
+                                  }
+                                }}
                                 placeholder="Enter full name"
                               />
+                              <div
+                                className="error text-danger"
+                                id="nameErr"
+                              ></div>
                             </div>
                           </div>
                           <div className="col-md-6">
